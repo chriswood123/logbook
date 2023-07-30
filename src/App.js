a85b86fbb77016a4526ff644bd94423508d4c989
Chris Wood <chriswood@Chriss-MacBook-Air.local>
2023-07-30 12:53:02 +0100

    Add AddEntry functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import Table from 'react-bootstrap/Table';
+import { useState } from "react";
 
 function Port({ port, portKey }) {
   return(
@@ -89,14 +90,113 @@ function Summary({ entries }) {
   return <div />;
 }
 
-function AddEntry() {
-  return <div />;
+function AddEntry({ entries, setEntries }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const formJson = Object.fromEntries(formData.entries());
+
+    let isTidal = false;
+    if (formJson.tidal === "on") {
+      isTidal = true;
+    }
+
+    const newEntry = {
+      dateFrom: formJson.dateFrom,
+      dateTo: formJson.dateTo,
+      vesselName: formJson.vesselName,
+      vesselLength: formJson.vesselLength,
+      vesselMake: formJson.vesselMake,
+      tidal: isTidal,
+      portsVisited: formJson.portsVisited.split(', '),
+      daysOnBoard: formJson.daysOnBoard,
+      maxWind: formJson.maxWind,
+      capacity: formJson.capacityOnBoard,
+      distanceLogged: formJson.distanceLogged,
+      nightHours: formJson.nightHours,
+      skipper: formJson.skipper
+    };
+
+    // Remove the example entry when adding
+    // the first real entry
+    if (entries.length === 1) {
+      console.log('1');
+      if (entries[0].example) {
+        setEntries([newEntry]);
+      } else {
+        setEntries([...entries, newEntry]);
+      }
+    } else {
+      setEntries([...entries, newEntry]);
+    }
+  }
+
+  return (
+    <form method="post" onSubmit={handleSubmit}>
+      <label>
+        Date From: <input name="dateFrom" />
+      </label>
+      <br />
+      <label>
+        Date To: <input name="dateTo" />
+      </label>
+      <br />
+      <label>
+        Vessel Name: <input name="vesselName" />
+      </label>
+      <br />
+      <label>
+        Vessel Make: <input name="vesselMake" />
+      </label>
+      <br />
+      <label>
+        Vessel Length: <input name="vesselLength" />
+      </label>
+      <br />
+      <label>
+        Ports Visited: <input name="portsVisited" />
+      </label>
+      <br />
+      <label>
+        Days On Board: <input name="daysOnBoard" />
+      </label>
+      <br />
+      <label>
+        Tidal: <input type="checkbox" name="tidal" defaultChecked={false} />
+      </label>
+      <br />
+      <label>
+        Max Wind: <input name="maxWind" />
+      </label>
+      <br />
+      <label>
+        Capacity On Board: <input name="capacityOnBoard" />
+      </label>
+      <br />
+      <label>
+        Distance Logged: <input name="distanceLogged" />
+      </label>
+      <br />
+      <label>
+        Night Hours: <input name="nightHours" />
+      </label>
+      <br />
+      <label>
+        Skipper: <input name="skipper" />
+      </label>
+      <br />
+      <button type="submit">Add Entry</button>
+    </form>
+  )
 }
 
 function LogBook({ initialEntries, prerequisites }) {
+  const [entries, setEntries] = useState(initialEntries);
+
   return (
     <>
-      <AddEntry />
+      <AddEntry entries={entries} setEntries={setEntries}/>
       <Summary entries={entries} />
       <Prerequisites entries={entries} prerequisites={prerequisites} />
       <EntryTable entries={entries} />
