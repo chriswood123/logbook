6dc19bf489cf902ca0b2d79b1ab4c5d5c5f8319c
Chris Wood <chriswood123@pm.me>
2023-08-09 21:02:24 +0100

    Add tidal calculations to Summary

--- src/Summary.js
+++ src/Summary.js
@@ -3,28 +3,52 @@ import { useState } from "react";
 import { Button } from "react-bootstrap";
 import { Collapse } from "react-bootstrap";
 
-function calculateMiles(entries, asSkipper=false) {
+function calculateNonTidalMiles(entries, asSkipper=false) {
     let totalMiles = 0;
     entries.forEach((entry) => {
-        if (asSkipper) {
-            // console.log('skipper')
-            // console.log('capacity' + entry["capacity"].toLowerCase)
-            // if (entry["capacity"].toLowerCase === "skipper") {
-            if (entry["capacity"] === "skipper") {
+        if (entry["tidal"] === false) {
+            if (asSkipper) {
+                if (entry["capacity"].toLowerCase() === "skipper") {
+                    totalMiles = totalMiles + parseInt(entry["distanceLogged"]);
+                }
+            } else {
                 totalMiles = totalMiles + parseInt(entry["distanceLogged"]);
             }
-        } else {
-            totalMiles = totalMiles + parseInt(entry["distanceLogged"]);
         }
     });
 
     return totalMiles;
 }
 
-function calculateNightHours(entries) {
+function calculateTidalMiles(entries, asSkipper=false) {
+    let totalMiles = 0;
+    entries.forEach((entry) => {
+        if (entry["tidal"] === true) {
+            if (asSkipper) {
+                if (entry["capacity"].toLowerCase() === "skipper") {
+                    totalMiles = totalMiles + parseInt(entry["distanceLogged"]);
+                }
+            } else {
+                totalMiles = totalMiles + parseInt(entry["distanceLogged"]);
+            }
+        }
+    });
+
+    return totalMiles;
+}
+
+function calculateNightHours(entries, tidal=false) {
     let totalNightHours = 0;
     entries.forEach((entry) => {
-        totalNightHours = totalNightHours + parseInt(entry["nightHours"])
+        if (tidal) {
+            if (entry["tidal"] === true) {
+                totalNightHours = totalNightHours + parseInt(entry["nightHours"]);
+            }
+        } else {
+            if (entry["tidal"] === false) {
+                totalNightHours = totalNightHours + parseInt(entry["nightHours"]);
+            }
+        }
     });
 
     return totalNightHours;
@@ -33,9 +57,17 @@ function calculateNightHours(entries) {
 export default function Summary({ entries }) {
     const [open, setOpen] = useState(false);
 
-    const totalMiles = calculateMiles(entries);
-    const totalMilesAsSkipper = calculateMiles(entries, true);
-    const totalNightHours = calculateNightHours(entries);
+    const totalMilesAsSkipperNonTidal = calculateNonTidalMiles(entries, true);
+    const totalMilesAsSkipperTidal = calculateTidalMiles(entries, true);
+    const totalMilesAsSkipper = totalMilesAsSkipperNonTidal + totalMilesAsSkipperTidal;
+
+    const totalNightHoursNonTidal = calculateNightHours(entries, false);
+    const totalNightHoursTidal = calculateNightHours(entries, true);
+    const totalNightHours = totalNightHoursNonTidal + totalNightHoursTidal;
+
+    const totalMilesNonTidal = calculateNonTidalMiles(entries, false);
+    const totalMilesTidal = calculateTidalMiles(entries, false);
+    const totalMiles = totalMilesNonTidal + totalMilesTidal;
 
     return(
         <>
@@ -66,15 +98,15 @@ export default function Summary({ entries }) {
                         </tr>
                         <tr>
                             <th scope="row" class="text-end">Tidal</th>
-                            <td class="text-center">{totalMiles}</td>
-                            <td class="text-center">{totalMilesAsSkipper}</td>
-                            <td class="text-center">{totalNightHours}</td>
+                            <td class="text-center">{totalMilesTidal}</td>
+                            <td class="text-center">{totalMilesAsSkipperTidal}</td>
+                            <td class="text-center">{totalNightHoursTidal}</td>
                         </tr>
                         <tr>
                             <th scope="row" class="text-end">Non-Tidal</th>
-                            <td class="text-center">{totalMiles}</td>
-                            <td class="text-center">{totalMilesAsSkipper}</td>
-                            <td class="text-center">{totalNightHours}</td>
+                            <td class="text-center">{totalMilesNonTidal}</td>
+                            <td class="text-center">{totalMilesAsSkipperNonTidal}</td>
+                            <td class="text-center">{totalNightHoursNonTidal}</td>
                         </tr>
                     </tbody>
                 </Table>
